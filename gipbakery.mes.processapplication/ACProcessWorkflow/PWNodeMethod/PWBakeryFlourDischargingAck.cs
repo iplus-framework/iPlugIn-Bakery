e6f74b3596dc7448975cd012ac27c080cdc744e3
Damir Lisak <dlisak@kajbum.com>
2021-07-23 18:59:36 +0200

    Automatic completion of next stages for bakery

--- gipbakery.mes.processapplication/ACProcessWorkflow/PWNodeMethod/PWBakeryFlourDischargingAck.cs
+++ gipbakery.mes.processapplication/ACProcessWorkflow/PWNodeMethod/PWBakeryFlourDischargingAck.cs
@@ -6,12 +6,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace gipbakery.mes.processapplication
 {
     [ACClassInfo(Const.PackName_VarioAutomation, "en{'Flour discharging acknowledge'}de{'Mehlaustragsquittung'}", Global.ACKinds.TPWNodeStatic, Global.ACStorableTypes.Optional, false, PWMethodVBBase.PWClassName, true)]
     public class PWBakeryFlourDischargingAck : PWNodeUserAck
     {
+        #region c'tors
         static PWBakeryFlourDischargingAck()
         {
             RegisterExecuteHandler(typeof(PWBakeryFlourDischargingAck), HandleExecuteACMethod_PWBakeryFlourDischargingAck);
@@ -23,6 +25,9 @@ namespace gipbakery.mes.processapplication
             method.ParameterValueList.Add(new ACValue("MessageText", typeof(string), "", Global.ParamOption.Required));
             paramTranslation.Add("MessageText", "en{'Question text'}de{'Abfragetext'}");
 
+            method.ParameterValueList.Add(new ACValue("SkipIfNoComp", typeof(bool), false, Global.ParamOption.Required));
+            paramTranslation.Add("SkipIfNoComp", "en{'Skip if no components dosed'}de{'Überspringe wenn keine Komponente dosiert'}");
+
             var wrapper = new ACMethodWrapper(method, "en{'Flour discharging acknowledge'}de{'Mehlaustragsquittung'}", typeof(PWBakeryFlourDischargingAck), paramTranslation, null);
             ACMethod.RegisterVirtualMethod(typeof(PWBakeryFlourDischargingAck), ACStateConst.SMStarting, wrapper);
         }
@@ -33,9 +38,54 @@ namespace gipbakery.mes.processapplication
             base(acType, content, parentACObject, parameter, acIdentifier)
         {
         }
+        #endregion
 
-        private IACContainerTNet<bool> _IsCoverDown;
 
+        #region Properties
+        protected bool SkipIfNoComp
+        {
+            get
+            {
+                var method = MyConfiguration;
+                if (method != null)
+                {
+                    var acValue = method.ParameterValueList.GetACValue("SkipIfNoComp");
+                    if (acValue != null)
+                    {
+                        return acValue.ParamAsBoolean;
+                    }
+                }
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// This property is a reference to the BakeryReceivingPoint.CoverDown-Property if a Source-Value is bound
+        /// </summary>
+        private IACContainerTNet<bool> _BoundCoverDownProperty;
+
+        bool? _HasRunSomeDosings;
+        public bool? HasRunSomeDosings
+        {
+            get
+            {
+                using (ACMonitor.Lock(_20015_LockValue))
+                {
+                    return _HasRunSomeDosings;
+                }
+            }
+            set
+            {
+                using (ACMonitor.Lock(_20015_LockValue))
+                {
+                    _HasRunSomeDosings = value;
+                }
+            }
+        }
+        #endregion
+
+        #region Methods
         [ACMethodState("en{'Executing'}de{'Ausführend'}", 20, true)]
         public override void SMStarting()
         {
@@ -52,6 +102,37 @@ namespace gipbakery.mes.processapplication
                 return;
             }
 
+            if (!HasRunSomeDosings.HasValue && SkipIfNoComp)
+            {
+                List<PWDosing> previousDosings = FindPredecessors<PWDosing>(false,
+                                                                    c => c is PWDosing,
+                                                                    c => (c is PWNodeOr && (c as PWNodeOr).PWPointIn.ConnectionList.Where(d => d.ValueT is PWDosingLoop).Any()) || c is PWDosing,
+                                                                    40);
+                if (previousDosings == null || !previousDosings.Any())
+                {
+                    List<PWDischarging> dischargings = FindPredecessors<PWDischarging>(false,
+                                                                    c => c is PWDischarging,
+                                                                    c => (c is PWNodeOr && (c as PWNodeOr).PWPointIn.ConnectionList.Where(d => d.ValueT is PWDosingLoop).Any()) || c is PWDischarging,
+                                                                    40);
+                    if (dischargings != null && dischargings.Any())
+                    {
+                        PWDischarging predecessorDis = dischargings.FirstOrDefault();
+                        if (predecessorDis != null)
+                        {
+                            previousDosings = PWDosing.FindPreviousDosingsInPWGroup<PWDosing>(predecessorDis);
+                        }
+                    }
+                }
+                HasRunSomeDosings = false;
+                if (previousDosings != null)
+                    HasRunSomeDosings = previousDosings.Where(c => c.HasRunSomeDosings).Any();
+                if (!HasRunSomeDosings.Value)
+                {
+                    AckStart();
+                    return;
+                }
+            }
+
             IACPropertyNetTarget isCoverDown = recvPoint.IsCoverDown as IACPropertyNetTarget;
             if (isCoverDown == null || isCoverDown.Source == null)
             {
@@ -60,14 +141,20 @@ namespace gipbakery.mes.processapplication
                 return;
             }
 
-            _IsCoverDown = isCoverDown as IACContainerTNet<bool>;
-            if (_IsCoverDown.ValueT)
+            bool resetCover = false;
+            if (IsSimulationOn && _BoundCoverDownProperty == null)
+                resetCover = true;
+
+            _BoundCoverDownProperty = isCoverDown as IACContainerTNet<bool>;
+            if (resetCover)
+                _BoundCoverDownProperty.ValueT = false;
+            else if (_BoundCoverDownProperty.ValueT)
             {
                 AckStart();
                 return;
             }
 
-            _IsCoverDown.PropertyChanged += _IsCoverDown_PropertyChanged;
+            _BoundCoverDownProperty.PropertyChanged += _IsCoverDown_PropertyChanged;
             base.SMRunning();
         }
 
@@ -87,7 +174,7 @@ namespace gipbakery.mes.processapplication
         {
             if (e.PropertyName == Const.ValueT)
             {
-                if (_IsCoverDown != null && _IsCoverDown.ValueT)
+                if (_BoundCoverDownProperty != null && _BoundCoverDownProperty.ValueT)
                 {
                     AckStart();
                 }
@@ -96,17 +183,19 @@ namespace gipbakery.mes.processapplication
 
         public override void AckStart()
         {
-            if (_IsCoverDown == null || _IsCoverDown.ValueT)
-                base.AckStart();
+            if (_BoundCoverDownProperty != null && !_BoundCoverDownProperty.ValueT)
+                _BoundCoverDownProperty.ValueT = true;
+            base.AckStart();
         }
 
         public void ResetMembers()
         {
-            if (_IsCoverDown != null)
+            if (_BoundCoverDownProperty != null)
             {
-                _IsCoverDown.PropertyChanged -= _IsCoverDown_PropertyChanged;
-                _IsCoverDown = null;
+                _BoundCoverDownProperty.PropertyChanged -= _IsCoverDown_PropertyChanged;
+                _BoundCoverDownProperty = null;
             }
+            HasRunSomeDosings = null;
         }
 
         public static bool HandleExecuteACMethod_PWBakeryFlourDischargingAck(out object result, IACComponent acComponent, string acMethodName, gip.core.datamodel.ACClassMethod acClassMethod, params object[] acParameter)
@@ -123,6 +212,23 @@ namespace gipbakery.mes.processapplication
             //}
             return HandleExecuteACMethod_PWNodeUserAck(out result, acComponent, acMethodName, acClassMethod, acParameter);
         }
+        #endregion
+
+        #region Diagnose
+        protected override void DumpPropertyList(XmlDocument doc, XmlElement xmlACPropertyList)
+        {
+            base.DumpPropertyList(doc, xmlACPropertyList);
+
+            XmlElement xmlChild = xmlACPropertyList["HasRunSomeDosings"];
+            if (xmlChild == null)
+            {
+                xmlChild = doc.CreateElement("HasRunSomeDosings");
+                if (xmlChild != null)
+                    xmlChild.InnerText = HasRunSomeDosings.ToString();
+                xmlACPropertyList.AppendChild(xmlChild);
+            }
+        }
+        #endregion
 
     }
 }
