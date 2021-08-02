9559146ae9f727b9a4fb487801cc948267432e3c
Ivan Hrastinski <ihrastinski@kajbum.com>
2021-08-02 21:57:09 +0200

    Cleaning

--- gipbakery.mes.processapplication/ACProcessWorkflow/PWNodeMethod/PWBakeryCleaning.cs
+++ gipbakery.mes.processapplication/ACProcessWorkflow/PWNodeMethod/PWBakeryCleaning.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using gip.core.autocomponent;
+using gip.core.datamodel;
+using gip.mes.processapplication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,20 @@ using System.Threading.Tasks;
 
 namespace gipbakery.mes.processapplication
 {
-    public class PWBakeryCleaning
+    [ACClassInfo(Const.PackName_VarioAutomation, "en{'PWBakeryCleaning'}de{'PWBakeryCleaning'}", Global.ACKinds.TPWNodeMethod, Global.ACStorableTypes.Optional, false, PWMethodVBBase.PWClassName, true)]
+    public class PWBakeryCleaning : PWNodeProcessMethod
     {
+        public PWBakeryCleaning(ACClass acType, IACObject content, IACObject parentACObject, ACValueList parameter, string acIdentifier = "") : 
+            base(acType, content, parentACObject, parameter, acIdentifier)
+        {
+        }
+
+        public const string PWClassName = "PWBakeryCleaning";
+
+        [ACMethodState("en{'Executing'}de{'Ausführend'}", 20, true)]
+        public override void SMStarting()
+        {
+            base.SMStarting();
+        }
     }
 }
--- gipbakery.mes.processapplication/PAProcessFunction/PAFBakeryCleaning.cs
+++ gipbakery.mes.processapplication/PAProcessFunction/PAFBakeryCleaning.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using gip.core.autocomponent;
+using gip.core.datamodel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,47 @@ using System.Threading.Tasks;
 
 namespace gipbakery.mes.processapplication
 {
-    public class PAFBakeryCleaning
+    [ACClassInfo(Const.PackName_VarioAutomation, "en{'Cleaning'}de{'Reinigen'}", Global.ACKinds.TPAProcessFunction, Global.ACStorableTypes.Required, false, PWBakeryCleaning.PWClassName, true)]
+    public class PAFBakeryCleaning : PAProcessFunction
     {
+        static PAFBakeryCleaning()
+        {
+            ACMethod.RegisterVirtualMethod(typeof(PAFBakeryCleaning), ACStateConst.TMStart, CreateVirtualMethod("BakeryCleaning", "en{'Cleaning'}de{'Reinigen'}", typeof(PWBakeryCleaning)));
+            RegisterExecuteHandler(typeof(PAFBakeryCleaning), HandleExecuteACMethod_PAFBakeryCleaning);
+        }
+
+
+
+        public PAFBakeryCleaning(ACClass acType, IACObject content, IACObject parentACObject, ACValueList parameter, string acIdentifier = "") : 
+            base(acType, content, parentACObject, parameter, acIdentifier)
+        {
+        }
+
+        public const string ClassName = "PAFBakeryCleaning";
+
+        [ACMethodAsync("Process", "en{'Start'}de{'Start'}", (short)MISort.Start, false)]
+        public override ACMethodEventArgs Start(ACMethod acMethod)
+        {
+            return base.Start(acMethod);
+        }
+
+        protected static ACMethodWrapper CreateVirtualMethod(string acIdentifier, string captionTranslation, Type pwClass)
+        {
+            ACMethod method = new ACMethod(acIdentifier);
+
+            Dictionary<string, string> paramTranslation = new Dictionary<string, string>();
+            method.ParameterValueList.Add(new ACValue("CleaningTarget", typeof(int), 0, Global.ParamOption.Required));
+            paramTranslation.Add("CleaningTarget", "en{'Cleaning target'}de{'Reinigungsziel'}");
+            method.ParameterValueList.Add(new ACValue("TargetQuantity", typeof(double), 0.0, Global.ParamOption.Required));
+            paramTranslation.Add("TargetQuantity", "en{'Target quantity'}de{'Sollmenge'}");
+
+
+            return new ACMethodWrapper(method, captionTranslation, pwClass, paramTranslation, null);
+        }
+
+        private static bool HandleExecuteACMethod_PAFBakeryCleaning(out object result, IACComponent acComponent, string acMethodName, ACClassMethod acClassMethod, object[] acParameter)
+        {
+            return HandleExecuteACMethod_PAProcessFunction(out result, acComponent, acMethodName, acClassMethod, acParameter);
+        }
     }
 }
