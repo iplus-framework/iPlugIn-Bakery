46ca606f54f4727e34f3bda23fa4fe5592a683b0
Ivan Hrastinski <ihrastinski@kajbum.com>
2021-08-05 08:30:34 +0200

    Serializers

---
+++ gipbakery.mes.processapplication/Converter/BakeryFuncSerial2006Cleaning.cs
@@ -0,0 +1,33 @@
+﻿using gip.core.communication;
+using gip.core.datamodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gipbakery.mes.processapplication
+{
+    public class BakeryFuncSerial2006Cleaning : ACSessionObjSerializer
+    {
+        public BakeryFuncSerial2006Cleaning(ACClass acType, IACObject content, IACObject parentACObject, ACValueList parameter, string acIdentifier = "") :
+            base(acType, content, parentACObject, parameter, acIdentifier)
+        {
+        }
+
+        public override bool IsSerializerFor(string typeOrACMethodName)
+        {
+            return MethodNameEquals(typeOrACMethodName, "BakeryCleaning");
+        }
+
+        public override object ReadObject(object complexObj, int dbNo, int offset, object miscParams)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool SendObject(object complexObj, int dbNo, int offset, object miscParams)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ gipbakery.mes.processapplication/Converter/BakeryFuncSerial2006Pumping.cs
@@ -0,0 +1,66 @@
+﻿using gip.core.communication;
+using gip.core.datamodel;
+using gip2006.variobatch.processapplication;
+using System;
+
+namespace gipbakery.mes.processapplication
+{
+    [ACClassInfo(Const.PackName_VarioAutomation, "en{'Serializer for Pumping over'}de{'Serialisierer für Umpumpen'}", Global.ACKinds.TACDAClass, Global.ACStorableTypes.Required, false, false)]
+    public class BakeryFuncSerial2006Pumping : ACSessionObjSerializer
+    {
+        public BakeryFuncSerial2006Pumping(ACClass acType, IACObject content, IACObject parentACObject, ACValueList parameter, string acIdentifier = "") :
+            base(acType, content, parentACObject, parameter, acIdentifier)
+        {
+        }
+
+        public override bool IsSerializerFor(string typeOrACMethodName)
+        {
+            return MethodNameEquals(typeOrACMethodName, "BakeryPumping");
+        }
+
+        public override object ReadObject(object complexObj, int dbNo, int offset, object miscParams)
+        {
+            return null;
+        }
+
+        public override bool SendObject(object complexObj, int dbNo, int offset, object miscParams)
+        {
+            S7TCPSession s7Session = ParentACComponent as S7TCPSession;
+            if (s7Session == null || complexObj == null)
+                return false;
+            if (!s7Session.PLCConn.IsConnected)
+                return false;
+            ACMethod request = complexObj as ACMethod;
+            if (request == null)
+                return false;
+
+            int iOffset = 0;
+
+            iOffset += gip.core.communication.ISOonTCP.Types.Real.Length; // TargetQuantity
+            iOffset += gip.core.communication.ISOonTCP.Types.Int.Length; // Source
+            iOffset += gip.core.communication.ISOonTCP.Types.Int.Length; // Destination
+
+            if (s7Session.HashCodeValidation != HashCodeValidationEnum.Off)
+                iOffset += gip.core.communication.ISOonTCP.Types.DInt.Length;
+
+            byte[] sendPackage1 = new byte[iOffset];
+            iOffset = 0;
+            if (s7Session.HashCodeValidation == HashCodeValidationEnum.Head || s7Session.HashCodeValidation == HashCodeValidationEnum.Head_WithRead)
+                iOffset += gip.core.communication.ISOonTCP.Types.DInt.Length;
+
+            Array.Copy(gip.core.communication.ISOonTCP.Types.Real.ToByteArray(request.ParameterValueList.GetDouble("TargetQuantity")),
+                0, sendPackage1, iOffset, gip.core.communication.ISOonTCP.Types.Real.Length);
+            iOffset += gip.core.communication.ISOonTCP.Types.Real.Length;
+
+            Array.Copy(gip.core.communication.ISOonTCP.Types.Int.ToByteArray(request.ParameterValueList.GetInt16("Source")),
+                0, sendPackage1, iOffset, gip.core.communication.ISOonTCP.Types.Int.Length);
+            iOffset += gip.core.communication.ISOonTCP.Types.Int.Length;
+
+            Array.Copy(gip.core.communication.ISOonTCP.Types.Int.ToByteArray(request.ParameterValueList.GetInt16("Destination")),
+                0, sendPackage1, iOffset, gip.core.communication.ISOonTCP.Types.Int.Length);
+            iOffset += gip.core.communication.ISOonTCP.Types.Int.Length;
+
+            return this.SendObjectToPLC(s7Session, request, sendPackage1, dbNo, offset, iOffset);
+        }
+    }
+}
