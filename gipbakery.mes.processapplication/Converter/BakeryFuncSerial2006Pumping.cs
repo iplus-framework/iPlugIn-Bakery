34d0ca89e99328530f108b7a0cf9b0f7b087b90c
Ivan Hrastinski <ihrastinski@kajbum.com>
2021-08-05 14:04:02 +0200

    Serializers

--- gipbakery.mes.processapplication/Converter/BakeryFuncSerial2006Pumping.cs
+++ gipbakery.mes.processapplication/Converter/BakeryFuncSerial2006Pumping.cs
@@ -1,4 +1,5 @@
 ﻿using gip.core.communication;
+using gip.core.communication.ISOonTCP;
 using gip.core.datamodel;
 using gip2006.variobatch.processapplication;
 using System;
@@ -20,7 +21,74 @@ namespace gipbakery.mes.processapplication
 
         public override object ReadObject(object complexObj, int dbNo, int offset, object miscParams)
         {
-            return null;
+            S7TCPSession s7Session = ParentACComponent as S7TCPSession;
+            if (s7Session == null || complexObj == null)
+                return null;
+            if (!s7Session.PLCConn.IsConnected)
+                return null;
+            ACMethod response = complexObj as ACMethod;
+            if (response == null)
+                return null;
+
+            ACChildInstanceInfo childInfo = null;
+            IACComponent invokerModule = null;
+            bool readParameter = false;
+            if (miscParams != null && miscParams is bool)
+                readParameter = (bool)miscParams;
+            else if (miscParams != null && miscParams is object[])
+            {
+                object[] paramArr = miscParams as object[];
+                childInfo = paramArr[0] as ACChildInstanceInfo;
+                if (childInfo != null)
+                {
+                    invokerModule = ACUrlCommand(childInfo.ACUrlParent) as IACComponent;
+                }
+                readParameter = (bool)paramArr[1];
+            }
+
+            if (readParameter)
+            {
+                int iOffset = 0;
+
+                iOffset += gip.core.communication.ISOonTCP.Types.Real.Length; // TargetQuantity
+                iOffset += gip.core.communication.ISOonTCP.Types.Int.Length; // Source
+                iOffset += gip.core.communication.ISOonTCP.Types.Int.Length; // Destination
+
+                OnReadObjectGetLength(response, dbNo, offset, miscParams, readParameter, ref iOffset);
+
+                byte[] readPackage1 = new byte[iOffset];
+
+                ErrorCode errCode = s7Session.PLCConn.ReadBytes(DataType.DataBlock, dbNo, offset, iOffset, out readPackage1);
+                if (errCode != ErrorCode.NoError)
+                    return null;
+
+                iOffset = 0;
+                if (s7Session.HashCodeValidation == HashCodeValidationEnum.Head || s7Session.HashCodeValidation == HashCodeValidationEnum.Head_WithRead)
+                    iOffset += gip.core.communication.ISOonTCP.Types.DInt.Length;
+
+                response.ParameterValueList.GetACValue("TargetQuantity").Value = gip.core.communication.ISOonTCP.Types.Real.FromByteArray(readPackage1, iOffset);
+                iOffset += gip.core.communication.ISOonTCP.Types.Real.Length;
+
+                response.ParameterValueList.GetACValue("Source").Value = gip.core.communication.ISOonTCP.Types.Int.FromByteArray(readPackage1, iOffset);
+                iOffset += gip.core.communication.ISOonTCP.Types.Int.Length;
+
+                response.ParameterValueList.GetACValue("Destination").Value = gip.core.communication.ISOonTCP.Types.Int.FromByteArray(readPackage1, iOffset);
+                iOffset += gip.core.communication.ISOonTCP.Types.Int.Length;
+
+                OnReadObjectAppend(response, dbNo, iOffset, miscParams, readPackage1, readParameter, ref iOffset);
+            }
+
+            return response;
+        }
+
+        protected virtual void OnReadObjectGetLength(ACMethod acMethod, int dbNo, int offset, object miscParams, bool readParameter, ref int iOffset, short telegramNo = 1)
+        {
+
+        }
+
+        protected virtual void OnReadObjectAppend(ACMethod acMethod, int dbNo, int offset, object miscParams, byte[] readPackage, bool readParameter, ref int iOffset, short telegramNo = 1)
+        {
+
         }
 
         public override bool SendObject(object complexObj, int dbNo, int offset, object miscParams)
