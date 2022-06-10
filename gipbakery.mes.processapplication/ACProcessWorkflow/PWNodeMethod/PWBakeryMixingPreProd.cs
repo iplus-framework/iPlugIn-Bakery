a1335793881bb58f5e7fa2dbbba0cb3d246ecf83
Damir Lisak <dlisak@kajbum.com>
2022-06-10 14:20:24 +0200

    Bugfix wait with dosing of fermentation-bin if other is currently active

--- gipbakery.mes.processapplication/ACProcessWorkflow/PWNodeMethod/PWBakeryMixingPreProd.cs
+++ gipbakery.mes.processapplication/ACProcessWorkflow/PWNodeMethod/PWBakeryMixingPreProd.cs
@@ -1,6 +1,7 @@
 ﻿using gip.core.autocomponent;
 using gip.core.datamodel;
 using gip.mes.processapplication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
@@ -35,6 +36,19 @@ namespace gipbakery.mes.processapplication
         {
         }
 
+        public override bool ACDeInit(bool deleteACClassTask = false)
+        {
+            _LastCheck = DateTime.MinValue;
+            return base.ACDeInit(deleteACClassTask);
+        }
+
+        public override void Recycle(IACObject content, IACObject parentACObject, ACValueList parameter, string acIdentifier = "")
+        {
+            _LastCheck = DateTime.MinValue;
+            StartingOrder.ValueT = null;
+            base.Recycle(content, parentACObject, parameter, acIdentifier);
+        }
+
         #endregion
 
         #region Properties
@@ -66,6 +80,12 @@ namespace gipbakery.mes.processapplication
         #endregion
 
         #region Methods
+        public override void SMIdle()
+        {
+            _LastCheck = DateTime.MinValue;
+            StartingOrder.ValueT = null;
+            base.SMIdle();
+        }
 
         [ACMethodState("en{'Executing'}de{'Ausführend'}", 20, true)]
         public override void SMStarting()
@@ -86,61 +106,68 @@ namespace gipbakery.mes.processapplication
             base.SMCompleted();
         }
 
+        private DateTime _LastCheck = DateTime.MinValue;
         private bool VerifyCanStart()
         {
-            if (DosingGroupNo > 0)
+            if (DosingGroupNo <= 0)
+                return true;
+            if (  _LastCheck > DateTime.MinValue
+                && DateTime.Now < _LastCheck.AddSeconds(10))
+                return false;
+
+            _LastCheck = DateTime.Now;
+            var possibleMixings = ApplicationManager.FindChildComponents<PWBakeryMixingPreProd>()
+                                                    .Where(x => x.ParentPWGroup != ParentPWGroup && x.DosingGroupNo == DosingGroupNo);
+            var dosings = possibleMixings.SelectMany(x => x.GetNextDosings());
+            bool anyRun = dosings.Any(c => c.CurrentACState == ACStateEnum.SMRunning 
+                                        || c.CurrentACState == ACStateEnum.SMStarting)
+                                        || possibleMixings.Any(c => c.CurrentACState == ACStateEnum.SMRunning);
+
+
+            if (!anyRun)
             {
-                var possibleMixings = ApplicationManager.FindChildComponents<PWBakeryMixingPreProd>().Where(x => x.ParentPWGroup != ParentPWGroup && x.DosingGroupNo == DosingGroupNo);
-                var dosings = possibleMixings.SelectMany(x => x.GetNextDosings());
-                bool anyRun = dosings.Any(c => c.CurrentACState == ACStateEnum.SMRunning || c.CurrentACState == ACStateEnum.SMStarting)
-                           || possibleMixings.Any(c => c.CurrentACState == ACStateEnum.SMRunning);
-
-
-                if (!anyRun)
+                var cleaningNodes = ApplicationManager.FindChildComponents<PWBakeryCleaning>();
+                if (cleaningNodes != null && cleaningNodes.Any())
                 {
-                    var cleaningNodes = ApplicationManager.FindChildComponents<PWBakeryCleaning>();
-                    if (cleaningNodes != null && cleaningNodes.Any())
-                    {
-                        anyRun = cleaningNodes.Any(c => c.CurrentACState == ACStateEnum.SMRunning || c.CurrentACState == ACStateEnum.SMStarting);
-                    }
+                    anyRun = cleaningNodes.Any(c => c.CurrentACState == ACStateEnum.SMRunning || c.CurrentACState == ACStateEnum.SMStarting);
                 }
+            }
 
-                if (StartingOrder.ValueT.HasValue && !anyRun)
-                {
-                    short? nextRun = possibleMixings.Where(x => x.StartingOrder.ValueT.HasValue).Min(c => c.StartingOrder.ValueT);
-                    if (nextRun == null || nextRun >= StartingOrder.ValueT)
-                        return true;
+            if (StartingOrder.ValueT.HasValue && !anyRun)
+            {
+                short? nextRun = possibleMixings.Where(x => x.StartingOrder.ValueT.HasValue).Min(c => c.StartingOrder.ValueT);
+                if (nextRun == null || nextRun >= StartingOrder.ValueT)
+                    return true;
 
-                    return false;
-                }
-                else
+                return false;
+            }
+            else
+            {
+                if (anyRun)
                 {
-                    if (anyRun)
+                    if (!StartingOrder.ValueT.HasValue)
                     {
-                        if (!StartingOrder.ValueT.HasValue)
+                        var waitingMixings = possibleMixings.Where(c => c.StartingOrder.ValueT != null).Select(x => x.StartingOrder.ValueT);
+                        if (waitingMixings != null && waitingMixings.Any())
                         {
-                            var waitingMixings = possibleMixings.Where(c => c.StartingOrder.ValueT != null).Select(x => x.StartingOrder.ValueT);
-                            if (waitingMixings != null && waitingMixings.Any())
-                            {
-                                short max = waitingMixings.Max(c => c.Value);
-                                StartingOrder.ValueT = (short)(max + 1);
-                            }
-                            else
-                            {
-                                StartingOrder.ValueT = 1;
-                            }
+                            short max = waitingMixings.Max(c => c.Value);
+                            StartingOrder.ValueT = (short)(max + 1);
                         }
-                        return false;
+                        else
+                        {
+                            StartingOrder.ValueT = 1;
+                        }
                     }
+                    return false;
                 }
             }
 
             return true;
         }
 
-        public IEnumerable<PWBakeryDosing> GetNextDosings()
+        public IEnumerable<PWBakeryDosingPreProd> GetNextDosings()
         {
-            return FindSuccessors<PWBakeryDosing>(true, c => c is PWBakeryDosing, null, 1);
+            return FindSuccessors<PWBakeryDosingPreProd>(true, c => c is PWBakeryDosingPreProd, null, 1);
         }
 
         protected override void DumpPropertyList(XmlDocument doc, XmlElement xmlACPropertyList)
