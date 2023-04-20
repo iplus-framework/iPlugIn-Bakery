8db134d6cf4603bdcc494edb9fc6a0bfc2ccd0ed
Ivan Hrastinski <ihrastinski@kajbum.com>
2023-04-20 11:47:26 +0200

    TasksOnHold PanPek

--- gipbakery.mes.processapplication/PAProcessFunction/PAFBakeryManualWeighing.cs
+++ gipbakery.mes.processapplication/PAProcessFunction/PAFBakeryManualWeighing.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace gipbakery.mes.processapplication
 {
-    [ACClassInfo(Const.PackName_VarioAutomation, "en{'Manual weighing bakery'}de{'Handzugabe Backerei'}", Global.ACKinds.TPAProcessFunction, Global.ACStorableTypes.Required, false, true, PWInfoACClass = PWManualWeighing.PWClassName, BSOConfig = "BakeryBSOManualWeighing", SortIndex = 100)]
+    [ACClassInfo(Const.PackName_VarioAutomation, "en{'Manual weighing bakery'}de{'Handzugabe Backerei'}", Global.ACKinds.TPAProcessFunction, Global.ACStorableTypes.Required, false, true, PWInfoACClass = PWManualWeighing.PWClassName, BSOConfig = nameof(BakeryBSOManualWeighing), SortIndex = 100)]
     public class PAFBakeryManualWeighing : PAFManualWeighing
     {
         public PAFBakeryManualWeighing(ACClass acType, IACObject content, IACObject parentACObject, ACValueList parameter, string acIdentifier = "") : 
