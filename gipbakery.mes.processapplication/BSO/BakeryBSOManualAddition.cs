a45281d9a56b6deea43b2f7f5f84378348f8ea97
aagincic <aleksandar.agincic@gmail.com>
2024-02-26 14:02:37 +0100

    iP-T-24-02-26-001 SchaefersLE in Work Center show ExternLotNo in lot selection

--- gipbakery.mes.processapplication/BSO/BakeryBSOManualAddition.cs
+++ gipbakery.mes.processapplication/BSO/BakeryBSOManualAddition.cs
@@ -1,10 +1,5 @@
 ﻿using gip.bso.manufacturing;
 using gip.core.datamodel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace gipbakery.mes.processapplication
 {
